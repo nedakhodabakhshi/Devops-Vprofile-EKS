ff0f5eb40c1010073ed2dd969f7351dc8e6b7aad
Neda Khodabakhshi <khodabakhshi.neda@gmail.com>
2025-02-10 14:09:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,6 @@ kubectl version --client
 
 
 #update awscli
-$ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
